ee57602585877d302c760d0acd83b81a54b11d71
Sergio Bazan <drakeb2008@hotmail.com>
2024-06-30 11:02:16 -0500

    add validation behavior

---
+++ Bookify.Application/Abstractions/Behaviors/ValidationBehavior.cs
@@ -0,0 +1,44 @@
+﻿using Bookify.Application.Abstractions.Messaging;
+using Bookify.Application.Exceptions;
+using FluentValidation;
+using MediatR;
+
+namespace Bookify.Application.Abstractions.Behaviors;
+
+public class ValidationBehavior<TRequest, TResponse> 
+    : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : ICommandBase
+{
+    private readonly IReadOnlyList<IValidator<TRequest>> _validators;
+
+    public ValidationBehavior(IReadOnlyList<IValidator<TRequest>> validators)
+    {
+        _validators = validators;
+    }
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        if (!_validators.Any())
+        {
+            return await next();
+        }
+
+        var context = new ValidationContext<TRequest>(request);
+
+        var validationErrors = _validators
+            .Select(validator => validator.Validate(context))
+            .Where(validationResult => validationResult.Errors.Any())
+            .SelectMany(validationResult => validationResult.Errors)
+            .Select(validationFailure => new ValidationError(
+                validationFailure.PropertyName,
+                validationFailure.ErrorMessage))
+            .ToList();
+
+        if (validationErrors.Any())
+        {
+            throw new Exceptions.ValidationException(validationErrors);
+        }
+
+        return await next();
+    }
+}
--- Bookify.Application/DependencyInjection.cs
+++ Bookify.Application/DependencyInjection.cs
@@ -1,5 +1,6 @@
 ﻿using Bookify.Application.Abstractions.Behaviors;
 using Bookify.Domain.Bookings;
+using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Bookify.Application;
@@ -8,12 +9,18 @@ public static class DependencyInjection
 {
     private static IServiceCollection AddApplication(this IServiceCollection services)
     {
+        var assembly = typeof(DependencyInjection).Assembly;
+
         services.AddMediatR(configuration =>
         {
-            configuration.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly);
+            configuration.RegisterServicesFromAssembly(assembly);
 
             configuration.AddOpenBehavior(typeof(LoggingBehavior<,>));
+           
+            configuration.AddOpenBehavior(typeof(ValidationBehavior<,>));
         });
+
+        services.AddValidatorsFromAssembly(assembly);
 
         services.AddTransient<PricingService>();
 
