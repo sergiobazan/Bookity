5c37d8e9505696bcb90947dec7607e7999dc58cf
Sergio Bazan <drakeb2008@hotmail.com>
2024-06-30 12:12:02 -0500

    add entities configurations

--- Bookify.Infrastructure/ApplicationDbContext.cs
+++ Bookify.Infrastructure/ApplicationDbContext.cs
@@ -10,4 +10,11 @@ public sealed class ApplicationDbContext : DbContext, IUnitOfWork
     {
         
     }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+
+        base.OnModelCreating(modelBuilder);
+    }
 }
---
+++ Bookify.Infrastructure/Configurations/ApartmentConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using Bookify.Domain.Apartments;
+using Bookify.Domain.Shared;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Bookify.Infrastructure.Configurations;
+
+internal sealed class ApartmentConfiguration : IEntityTypeConfiguration<Apartment>
+{
+    public void Configure(EntityTypeBuilder<Apartment> builder)
+    {
+        builder.HasKey(a => a.Id);
+
+        builder.ToTable("apartments");
+
+        builder.OwnsOne(a => a.Address);
+
+        builder.Property(a => a.Name)
+            .HasMaxLength(200)
+            .HasConversion(name => name.Value, value => new Name(value));
+
+        builder.Property(a => a.Description)
+            .HasMaxLength(2000)
+            .HasConversion(description => description.Value, value => new Description(value));
+
+        builder.OwnsOne(a => a.Price, priceBuilder =>
+        {
+            priceBuilder.Property(money => money.Currency)
+                .HasConversion(currency => currency.Code, code => Currency.FromCode(code));
+        });
+
+        builder.OwnsOne(a => a.CleaningFee, priceBuilder =>
+        {
+            priceBuilder.Property(money => money.Currency)
+                .HasConversion(currency => currency.Code, code => Currency.FromCode(code));
+        });
+    }
+}
