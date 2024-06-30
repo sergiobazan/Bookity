72c3df852750746205e5ccc4aef8db50ad723af2
Sergio Bazan <drakeb2008@hotmail.com>
2024-06-30 11:17:01 -0500

    add infrastructure layer configurations

---
+++ Bookify.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Bookify.Domain.Abstractions;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bookify.Infrastructure;
+
+public sealed class ApplicationDbContext : DbContext, IUnitOfWork
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+        
+    }
+}
---
+++ Bookify.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,29 @@
+﻿using Bookify.Application.Abstractions.Clock;
+using Bookify.Application.Abstractions.Email;
+using Bookify.Infrastructure.Clock;
+using Bookify.Infrastructure.Email;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+
+namespace Bookify.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddTransient<IDateTimeProvider, DateTimeProvider>();
+        
+        services.AddTransient<IEmailService, EmailService>();
+
+        var connectionString = configuration.GetConnectionString("Database") ??
+                               throw new ArgumentNullException(nameof(configuration));
+
+        services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            options.UseNpgsql(connectionString).UseSnakeCaseNamingConvention();
+        });
+
+        return services;
+    }
+}
