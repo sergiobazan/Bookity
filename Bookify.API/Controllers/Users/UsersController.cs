3bb5c1b3e2a618ec7e4d2499e90209832e4439fe
Sergio Bazan <drakeb2008@hotmail.com>
2024-07-02 15:07:33 -0500

    add login with keycloak

--- Bookify.API/Controllers/Users/UsersController.cs
+++ Bookify.API/Controllers/Users/UsersController.cs
@@ -1,4 +1,5 @@
-﻿using Bookify.Application.Users.RegisterUser;
+﻿using Bookify.Application.Users.LogInUser;
+using Bookify.Application.Users.RegisterUser;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -29,6 +30,24 @@ public class UsersController : ControllerBase
             request.Password);
 
         var result = await _sender.Send(command, cancellationToken);
+
+        return Ok(result.Value);
+    }
+
+    [AllowAnonymous]
+    [HttpPost("login")]
+    public async Task<IActionResult> LogIn(
+        LogInUserRequest request,
+        CancellationToken cancellationToken)
+    {
+        var command = new LogInUserCommand(request.Email, request.Password);
+
+        var result = await _sender.Send(command, cancellationToken);
+
+        if (result.IsFailure)
+        {
+            return Unauthorized(result.Error);
+        }
 
         return Ok(result.Value);
     }
--- Bookify.Infrastructure/DependencyInjection.cs
+++ Bookify.Infrastructure/DependencyInjection.cs
@@ -30,6 +30,13 @@ public static class DependencyInjection
 
         AddPersistence(services, configuration);
 
+        AddAuthentication(services, configuration);
+
+        return services;
+    }
+
+    private static void AddAuthentication(IServiceCollection services, IConfiguration configuration)
+    {
         services
             .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
             .AddJwtBearer();
@@ -50,7 +57,12 @@ public static class DependencyInjection
             })
             .AddHttpMessageHandler<AdminAuthorizationDelegatingHandler>();
 
-        return services;
+        services.AddHttpClient<IJwtService, JwtService>((serviceProvider, httpClient) =>
+        {
+            var keycloakOptions = serviceProvider.GetRequiredService<IOptions<KeycloakOptions>>().Value;
+
+            httpClient.BaseAddress = new Uri(keycloakOptions.TokenUrl);
+        });
     }
 
     private static void AddPersistence(IServiceCollection services, IConfiguration configuration)
